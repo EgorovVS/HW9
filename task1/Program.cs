ca69aba84f034542d140d5c674459de66e61776b
Spoon2 <viktor.spoon2@gmail.com>
2022-04-16 02:29:01 +0400

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Задайте значения M и N. Напишите программу, которая выведет все чётные 
+натуральные числа в промежутке от M до N используя рекурсию. */
+
+int M = Prompt("Введите число M ");
+int N = Prompt("Введите число N");
+PrintEven(M,N);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+void PrintEven (int M, int N)
+{
+    if(M>N) return;
+    if (M % 2 == 0)
+    Console.Write(M + " ");
+    PrintEven(M+1, N);
+}
