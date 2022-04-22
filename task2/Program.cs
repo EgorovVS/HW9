07a33012059166531adc3ebad57dc747aad2696e
Spoon2 <viktor.spoon2@gmail.com>
2022-04-22 23:35:14 +0400

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+элементов в промежутке от M до N используя рекурсию. */
+
+int M = Prompt("Введите число M ");
+int N = Prompt("Введите число N");
+PrintNaturalSum(M,N);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string strInput = Console.ReadLine();
+    int answer = int.Parse(strInput);
+    return answer;
+}
+
+void PrintNaturalSum (int M, int N)
+{   
+    int sum = 0;
+    if(M>N) return;
+    if (M!=0)
+    {
+    sum = sum + M;
+    }
+    PrintNaturalSum(sum, M+1);
+}
